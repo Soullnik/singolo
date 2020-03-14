e21a6aed15592398ab4857d5b8b1e5e52e63eaa4
Soullnik <Soullnik96@gmail.com>
2020-03-14 12:44:18 +0300

    feat: create slider

--- script.js
+++ script.js
@@ -1,6 +1,71 @@
-let swipeCounter = 0;
-let itemContainer = document.querySelectorAll('.item_container');
+let currentItem = 0;
+let isEnabled = true;
+let items = document.querySelectorAll('.item_container');
 
+
+const changeCurrentItem = (n) => {
+  currentItem = (n + items.length) % items.length;
+}
+
+const hideItem = (direction) => {
+  isEnabled = false;
+  items[currentItem].classList.add(direction)
+  items[currentItem].addEventListener('animationend', function() {
+    this.classList.remove('item_container--active', direction);
+    this.classList.add('item_container--next', direction)
+  })
+}
+
+const showItem = (direction) => {
+  items[currentItem].classList.add('item_container--next', direction)
+  items[currentItem].addEventListener('animationend', function() {
+    this.classList.remove('item_container--next', direction);
+    this.classList.add('item_container--active', direction);
+    isEnabled = true;
+  })
+}
+
+const previousItem = (n) => {
+  hideItem('to_right')
+  changeCurrentItem(n - 1)
+  showItem('from_left')
+}
+
+const nextItem = (n) => {
+  hideItem('to_left')
+  changeCurrentItem(n + 1)
+  showItem('from_right')
+}
+
+document.querySelector('.slider_container--arrow-previous').addEventListener('click', function(event) {
+  console.log(event.target)
+  if (isEnabled) {
+    previousItem(currentItem)
+  }
+})
+
+document.querySelector('.slider_container--arrow-next').addEventListener('click', function(event) {
+  console.log(event.target)
+  if (isEnabled) {
+    nextItem(currentItem)
+  }
+}) 
+
+// const swiper = (event) => {
+//   if (event.target.classList.contains('slider_container--arrow')) {
+//     const interSwipe = setInterval (() => {
+//       items.forEach((elem) => {
+//         console.log(elem.style.right)
+//         if (getComputedStyle(elem).right === '1530px') {
+//           clearInterval(interSwipe);
+//           return;  
+//         }
+//         swipeCounter += 1;
+//         elem.style.right = `${swipeCounter}px`;
+//       })
+//     }, 1000)
+//   }
+// }; 
 
 const menuScroll = (event) => {
   if(event.target.tagName === 'A')
@@ -28,42 +93,12 @@ const screenActive = (event) => {
   }
 };
 
-const swiper = (event) => {
-  if (event.target.classList.contains('slider_container--arrow')) {
-    const interSwipe = setInterval (() => {
-      document.querySelectorAll('.item_container').forEach((elem) => {
-        console.log(elem.style.right)
-        if (elem.style.right === '1530px') {
-          clearInterval(interSwipe);
-          return;  
-        }
-        swipeCounter += 1;
-        console.log(elem.style.right)
-        console.log(elem.style)
-        console.log(elem)
-        // elem.style.right = `${swipeCounter}px`;
-      })
-    }, 1000)
-  }
-}; 
-
-
-
-
-
-
-
-// phoneButton.addEventListener('click', function (event) {
-    
-    
-// })
-
 const init = () => {
   menu.addEventListener('click', menuScroll)
-  itemContainer.forEach((elem) => {
+  items.forEach((elem) => {
     elem.addEventListener('click', screenActive)
   })
-  slider.addEventListener('click', swiper)
+  // slider.addEventListener('click', swiper)
 }
 
 init();
