9f744fd599d5366b8e7125be5e688da65cca91e0
Soullnik <Soullnik96@gmail.com>
2020-03-24 15:03:24 +0300

    feat; touch swiper add

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const messageDescription = createDomNode('messageDescription', 'p', 'message--de
 const messageBtn = createDomNode('messageBtn', 'button', 'message--agree-hidden');
 const burgerBtn = document.querySelector('.burger');
 const headerBox = document.querySelector('.header_container');
+const swipeElement = document.querySelector('.slider_container');
 
 let currentItem = 0;
 let isEnabled = true;
@@ -257,7 +258,93 @@ function burgerHeandler(event) {
   }
 }
 
+
+const swipeDetected = (el) => {
+  console.log('sfsdf')
+
+  let surface = el;
+  let startX = 0;
+  let startY = 0;
+  let distX = 0;
+  let distY = 0;
+
+  let startTime = 0;
+  let elapsedTime = 0;
+
+  let threshold = 150;
+  let restraint = 100;
+  let allowedTime = 300;
+
+  surface.addEventListener('mousedown', function(e) {
+    startX = e.pageX;
+    startY = e.pageY;
+    startTime = new Date().getTime();
+    e.preventDefault();
+  })
+
+  surface.addEventListener('mouseup', function(e) {
+    distX = e.pageX - startX;
+    distY = e.pageY - startY;
+    elapsedTime = new Date().getTime() - startTime;
+
+    if (elapsedTime <= allowedTime) {
+      if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+        if (distX > 0) {
+          if (isEnabled) {
+            previousItem(currentItem);
+          }
+        } else {
+          if (isEnabled) {
+            nextItem(currentItem);
+          }
+        }
+      }
+    }
+
+    e.preventDefault();
+  })
+
+  surface.addEventListener('touchstart', function(e) {
+    let touchObj = e.changedTouches[0]; 
+    startX = touchObj.pageX;
+    startY = touchObj.pageY;
+    startTime = new Date().getTime();
+    e.preventDefault();
+  })
+
+  surface.addEventListener('touchmove', function(e) {
+    e.preventDefault();
+  })
+
+  surface.addEventListener('touchend', function(e) {
+    let touchObj = e.changedTouches[0]; 
+    distX = touchObj.pageX - startX;
+    distY = touchObj.pageY - startY;
+    elapsedTime = new Date().getTime() - startTime;
+
+    if (elapsedTime <= allowedTime) {
+      if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+        if (distX > 0) {
+          if (isEnabled) {
+            previousItem(currentItem);
+          }
+        } else {
+          if (isEnabled) {
+            nextItem(currentItem);
+          }
+        }
+      }
+    }
+
+    e.preventDefault();
+  })
+} 
+
+
+
 const init = () => {
+  swipeDetected(swipeElement);
+
   menu.addEventListener('click', menuScrollHandler)
   
   items.forEach((elem) => {
@@ -281,8 +368,6 @@ const init = () => {
   SubmitBtn.addEventListener('click', btnHeandler)
 
   burgerBtn.addEventListener('click', burgerHeandler)
-
-
 }
 
 init();
